ed35ce00b551b991d53e0cce7e9a41fa5466eba9
aimenkhakwani <akhakwani@phase2technology.com>
2020-05-07 04:21:01 -0500

    Start creating the quiz

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
+import quizQuestions from './data/quizQuestions';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      questionId: 1,
+      question: '',
+      answerOptions: [],
+      result: '',
+      endResult: '',
+    };
+
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+  }
+
+  componentDidMount() {
+    const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));
+
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0]
+    });
+  }
+
+  shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: quizQuestions[counter].question,
+      answerOptions: quizQuestions[counter].answers,
+      answer: ''
+    });
+  }
+
+  setUserAnswer(answer) {
+    this.setState((state) => ({
+      result: this.state.result + answer
+    }));
+  }
+
+  setResults () {
+    this.setState({ endResult: this.state.result });
+  }
+
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+    if (this.state.questionId < quizQuestions.length) {
+      setTimeout(() => this.setNextQuestion(), 300);
+    } else {
+      setTimeout(() => this.setResults(), 300);
+    }
+  }
+
+  renderQuiz() {
+    return (
+      <Quiz
+        answer={this.state.answer}
+        answerOptions={this.state.answerOptions}
+        questionId={this.state.questionId}
+        question={this.state.question}
+        onAnswerSelected={this.handleAnswerSelected}
+      />
+    );
+  }
+
+  renderResult() {
+    return (
+      <Result quizResult={this.state.endResult} />
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Quiz</h2>
+        </div>
+        {this.state.endResult ? this.renderResult() : this.renderQuiz()}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data/quizQuestions.js
@@ -0,0 +1,34 @@
+var quizQuestions = [
+  {
+    question: "Choose an ancient culture?",
+    answers: [
+      {
+        type: "aztec",
+        content: "Aztecs"
+      },
+      {
+        type: "mesopotamian",
+        content: "Mesopotamia"
+      },
+    ]
+  },
+  {
+    question: "What is your social status?",
+    answers: [
+      {
+        type: " nobleman",
+        content: "Nobleman"
+      },
+      {
+        type: " warrior",
+        content: "Warrior"
+      },
+      {
+        type: " commoner",
+        content: "Commoner"
+      }
+    ]
+  }
+];
+
+export default quizQuestions;
