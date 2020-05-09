bb81a9eee469af37061e5d72e5040be5686ed58e
aimenkhakwani <akhakwani@phase2technology.com>
2020-05-09 06:39:40 -0500

    Updates

---
+++ precache-manifest.3c9a90d58b5122db2f89157398f0499f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac90e2133d3ec5c993c99b4ddc190a6a",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "98af6f443fe978b54321",
+    "url": "/react-quiz/static/css/main.f4b3f9b3.chunk.css"
+  },
+  {
+    "revision": "393b6cbc5673c82431f0",
+    "url": "/react-quiz/static/js/2.f3312dc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz/static/js/2.f3312dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98af6f443fe978b54321",
+    "url": "/react-quiz/static/js/main.f1df598a.chunk.js"
+  },
+  {
+    "revision": "628b30ad5d2631412144",
+    "url": "/react-quiz/static/js/runtime-main.62206730.js"
+  }
+]);
